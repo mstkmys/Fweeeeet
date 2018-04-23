4011950c13dd0e8dfb30175aab5276d15c9ec23f
Masataka Miyoshi <masataka.ehime.univ@gmail.com>
2018-04-24 04:28:43 +0900

    初期設定を追加

---
+++ Fweeeeet/Classes/AppDelegate.swift
@@ -0,0 +1,43 @@
+//
+//  AppDelegate.swift
+//  Fweeeeet
+//
+//  Created by Miyoshi Masataka on 2018/04/24.
+//  Copyright © 2018年 Masataka Miyoshi. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        self.window = UIWindow(frame: UIScreen.main.bounds)
+        self.window?.makeKeyAndVisible()
+        self.window?.rootViewController = ViewController()
+        return true
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
